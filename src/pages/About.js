da380070f0cdf954a2ffd036e045e6a9349acbcf
Emily Chan <emilychan@ibm.com>
2023-06-26 16:36:12 -0700

    update About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "../assets/profile_pic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {profile_pic}
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Emily Chan</div>
+        <div className="brief_description">
+          I enjoy baking and traveling!
+        </div>
+      </div>
+    </div>
+  </div>
     )
   }
 }
